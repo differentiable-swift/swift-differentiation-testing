61024a3ad29fefff18e5042524d7d30b3aab05bc
Jaap Wijnen <jaap@passivelogic.com>
2024-11-19 15:34:59 +0100

    create validation testing swift package with one passing and one failing test

---
+++ RuntimeValidationTests/Tests/ValidationTests/ValidationTests.swift
@@ -0,0 +1,15 @@
+import Testing
+
+@Suite("Pipeline validation tests")
+struct SanityChecksTests {
+    @Test("Test that always passes") func alwaysPass() throws {
+        #expect(true)
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test("Test that always fails") func alwaysFail() throws {
+        withKnownIssue {
+            #expect(Bool(false))
+        }
+    }
+}
