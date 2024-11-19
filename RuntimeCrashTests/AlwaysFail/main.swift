f8bd16d9559ffc96aea57be5dfdb4ed1cb632033
Jaap Wijnen <jaap@passivelogic.com>
2024-11-19 15:33:05 +0100

    set up one passing and one failing runtime crash test

---
+++ RuntimeCrashTests/AlwaysFail/main.swift
@@ -0,0 +1,2 @@
+// This file contains failing code by design to make it compile successfully and crash at runtime to validate the runtimecrash test pipeline.
+fatalError("This is supposed to fail at runtime")
