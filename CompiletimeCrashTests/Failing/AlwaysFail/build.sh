3de592c88b446e48fefd6c70bcd9438fa9c5d3ed
Jaap Wijnen <jaap@passivelogic.com>
2024-12-24 13:55:16 +0100

    feat: make test results more obvious based on folder structure

--- CompiletimeCrashTests/Failing/AlwaysFail/build.sh
+++ CompiletimeCrashTests/Failing/AlwaysFail/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 swiftc main.swift -o main
 RETURN_CODE=$?
-EXPECTED_RETURN_CODE=0
--- RuntimeCrashTests/run-runtime-crash-tests.sh
+++ RuntimeCrashTests/run-runtime-crash-tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 echo -e "Running Runtime Crash tests\n"
 
-for folder in $(find . -type d -mindepth 1); do
+cd Passing
+echo -e "Running Passing Runtime Crash tests\n"
+for folder in $(find . -type d -depth 1); do
     cd "$folder" # navigate to current testing folder
     
     echo "Building $folder"
@@ -9,35 +11,46 @@ for folder in $(find . -type d -mindepth 1); do
     echo "Finished building $folder"
     
     echo "Running $folder"
-    source ./run.sh # is expected to set EXPECTED_RETURN_CODE and RETURN_CODE variables
+    source ./run.sh # is expected to set RETURN_CODE variable
+    echo "Finished running $folder"
+    
+    cd - > /dev/null # navigate back to previous folder
+    
+    # script expected to succeed
+    if [ $RETURN_CODE -eq 0 ]; then
+        # script succeeded
+        echo -e "$folder passed as expected\n"
+    else
+        # script failed unexpectedly
+        echo -e "$folder failed unexpectedly\n"
+        exit 1
+    fi
+done
+    
+cd ../Failing
+echo -e "Running Failing Runtime Crash tests\n"
+for folder in $(find . -type d -depth 1); do
+    cd "$folder" # navigate to current testing folder
+    
+    echo "Building $folder"
+    ./build.sh || exit 1 # This is not supposed to fail and therefore should fail this test
+    echo "Finished building $folder"
+    
+    echo "Running $folder"
+    source ./run.sh # is expected to set RETURN_CODE variable
     echo "Finished running $folder"
     
     cd - > /dev/null # navigate back to previous folder
 
-    # Check if results match expected results, if not, exit with error
-    if [ $EXPECTED_RETURN_CODE -eq 0 ]; then
-        # script expected to succeed
-        if [ $RETURN_CODE -eq 0 ]; then
-            # script succeeded
-            echo "$folder passed as expected"
-        else
-            # script failed unexpectedly
-            echo "$folder failed unexpectedly"
-            exit 1
-        fi
+    # script expected to fail
+    if [ $RETURN_CODE -eq 0 ]; then
+        # script passed unexpectedly
+        echo -e "$folder passed unexpectedly\n"
+        exit 1
     else
-        # script expected to fail
-        if [ $RETURN_CODE -eq 0 ]; then
-            # script passed unexpectedly
-            echo "$folder passed unexpectedly"
-            exit 1
-        else
-            # script failed as expected
-            echo "$folder failed as expected"
-        fi
+        # script failed as expected
+        echo -e "$folder failed as expected\n"
     fi
-    
-    echo -e "\n"
 done
 
 echo "Finished running all Runtime Crash tests"
