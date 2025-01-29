c188ce859fceee78fc713eb9a385a2365534740b
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-01-29 14:40:02 +0100

    feat: pre 6.0 crashers now fixed in 6.0.3

---
+++ CompiletimeCrashTests/Passing/c002-differentiation-transform-library-evoluation-leaked-owned-values-never-consumed/main.swift
@@ -0,0 +1,37 @@
+import _Differentiation
+
+public class Tracked<T> {}
+extension Tracked: Differentiable where T: Differentiable {}
+
+@differentiable
+func callback(_ x: inout Tracked<Float>.TangentVector) {}
+
+public extension Differentiable {
+  /// Applies the given closure to the derivative of `self`.
+  ///
+  /// Returns `self` like an identity function. When the return value is used in
+  /// a context where it is differentiated with respect to, applies the given
+  /// closure to the derivative of the return value.
+  @inlinable
+  @differentiable(reverse, wrt: self)
+  func withDerivative(_ body: @escaping (inout TangentVector) -> Void) -> Self {
+    return self
+  }
+
+  @inlinable
+  @derivative(of: withDerivative)
+  internal func _vjpWithDerivative(
+    _ body: @escaping (inout TangentVector) -> Void
+  ) -> (value: Self, pullback: (TangentVector) -> TangentVector) {
+    return (self, { grad in
+      var grad = grad
+      body(&grad)
+      return grad
+    })
+  }
+}
+
+@differentiable
+func caller(_ x: Tracked<Float>) -> Tracked<Float> {
+  return x.withDerivative(callback)
+}
