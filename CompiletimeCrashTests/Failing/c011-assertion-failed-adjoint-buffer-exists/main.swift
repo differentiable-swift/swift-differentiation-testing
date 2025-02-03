c617dbd81c91dc68f8fe652c318a048bea95a5aa
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-02-03 14:59:28 +0100

    feat: more failing single-file reproducers

---
+++ CompiletimeCrashTests/Failing/c011-assertion-failed-adjoint-buffer-exists/main.swift
@@ -0,0 +1,11 @@
+import _Differentiation
+
+// https://github.com/swiftlang/swift/issues/75280
+// https://github.com/swiftlang/swift/issues/79051
+// Used to crash, compilation fails now: not differentiable
+
+@differentiable(reverse) func a<F, A>(_ f: F?, c: @differentiable(reverse) (F) -> A) -> A?
+where F: Differentiable, A: Differentiable {
+    guard let f else { return nil }
+    return c(f)
+}
