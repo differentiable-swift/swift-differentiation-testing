f375dd4612ae9ba788d8faebd69adc021c2a5931
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-03-04 17:57:47 +0100

    fix: syntax error + assign SDKROOT only for macos

--- CompiletimeCrashTests/run-compiletime-crash-tests.sh
+++ CompiletimeCrashTests/run-compiletime-crash-tests.sh
@@ -9,7 +9,7 @@ for folder in $(find . -type d -mindepth 1 -maxdepth 1); do
     source ./build.sh # is expected to set RETURN_CODE variable
     echo "Finished building $folder"
     cd - > /dev/null # navigate back to previous folder
-    
+
     # script expected to succeed
     if [ $RETURN_CODE -eq 0 ]; then
         # script succeeded
@@ -25,7 +25,10 @@ cd ../Failing
 echo -e "Running Failing Compiletime Crash tests\n"
 for folder in $(find . -type d -mindepth 1 -maxdepth 1); do
     cd "$folder" # navigate to current testing folder
-    echo "Building $folder"export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    echo "Building $folder"
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    fi
     source ./build.sh # is expected to set RETURN_CODE variable
     echo "Finished building $folder"
     cd - > /dev/null # navigate back to previous folder
