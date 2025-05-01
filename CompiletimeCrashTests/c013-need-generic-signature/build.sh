834ccb96bbf7ce81c8f5c23abb82e8387999f2bb
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-05-01 13:40:34 -0600

    fix: remove no-longer-required args from check script

--- CompiletimeCrashTests/c013-need-generic-signature/build.sh
+++ CompiletimeCrashTests/c013-need-generic-signature/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-swift build -v 2>&1 | ../check.py "expected-${SWIFT_VERSION}-`uname -s`.txt"
+swift build -v 2>&1 | ../check.py
 RETURN_CODE=$?
--- CompiletimeCrashTests/c027-nested-differentiation-of-extension-method-optimized/build.sh
+++ CompiletimeCrashTests/c027-nested-differentiation-of-extension-method-optimized/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # NB: must be optimized build
-swiftc -O main.swift -o main 2>&1 | ../check.py "expected-${SWIFT_VERSION}-`uname -s`.txt"
+swiftc -O main.swift -o main 2>&1 | ../check.py
 RETURN_CODE=$?
