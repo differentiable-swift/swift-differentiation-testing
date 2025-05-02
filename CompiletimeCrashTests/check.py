aed50e3f759dd219724d8973a5dd7a94e0e8e1bc
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-05-02 14:33:57 -0600

    refactor: move all output checking inside `check`

--- CompiletimeCrashTests/check.py
+++ CompiletimeCrashTests/check.py
@@ -24,17 +24,27 @@ def errprint(*args, **kwargs):
     print(*args, **kwargs, file=stderr)
 
 
-def check(expected: [str], found: [str]):
+def check(expected: [str], found: [str]) -> ReproducerType:
+    reproducer_type = ReproducerType.parse(expected[0])
+    match reproducer_type:
+        case None:
+            raise ValueError(f"Expected a reproducer type header of the form {HEADER_REGEX}.")
+        case ReproducerType.OK as ok:
+            if len(expected) > 1:
+                raise ValueError(f'Reproducer type is {ok}, but found expected output {expected[1:]}')
+            elif not compiler_output:
+                return ok
+    expected = expected[1:]
     idx = 0
     expected_line = expected[idx]
     for line in found:
         if expected_line in line:
             idx += 1
             if idx == len(expected):
-                exit(0)
+                return reproducer_type
             expected_line = expected[idx]
         if idx == len(expected):
-            exit(0)
+            return reproducer_type
     errprint(f"FAIL: \"{expected_line} not found in {found}\"")
     exit(2)
 
@@ -43,26 +53,21 @@ def main():
     compiler_output = stdin.readlines()
     if not compiler_output and len(argv) == 1:
         # no ground truth file => OK
-        exit(0)
-
+        print("OK")
+        return
+    elif len(argv) == 1:
+        raise ValueError(f"No ground truth file provided (i.e. expected no crash or error), but compiler produced some output: \
+        {'\n'.join(compiler_output)}")
     # read ground truth file into list of lines
     expected_output = []
     ground_truth_filename = argv[1]
     with open(ground_truth_filename) as file:
         while line := file.readline():
             expected_output.append(line.rstrip())
-
-    match ReproducerType.parse(expected_output[0]):
-        case None:
-            raise ValueError(f"Expected a reproducer type header of the form {HEADER_REGEX}.")
-        case ReproducerType.OK as ok:
-            if len(expected_output) > 1:
-                raise ValueError(f'Reproducer type is {ok}, but found expected output {expected[1:]}')
-            elif not compiler_output:
-                exit(0)
-
+    
+    # TODO: add the print when the shell script has been changed accordingly
     # minus the header from `expected_output`
-    check(expected_output[1:], compiler_output)
+    # print(check(expected_output, compiler_output))
 
 
 if __name__ == '__main__':
