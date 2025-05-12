6dc61f80a917eb7c9f7d1ba84884146243403328
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-05-12 14:41:13 +0200

    refactor: `RETURN_CODE` no longer required in build script

--- CompiletimeCrashTests/c013-need-generic-signature/build.sh
+++ CompiletimeCrashTests/c013-need-generic-signature/build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 swift build -v 2>&1 | ../check.py
-RETURN_CODE=$?
--- CompiletimeCrashTests/c027-nested-differentiation-of-extension-method-optimized/build.sh
+++ CompiletimeCrashTests/c027-nested-differentiation-of-extension-method-optimized/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # NB: must be optimized build
 swiftc -O main.swift -o main 2>&1 | ../check.py
-RETURN_CODE=$?
