ccff80d66c82fc695eb2d0f52d444fbe63e54a1f
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-05-15 19:05:53 +0200

    revert: associative arrays are not available in bash 3.2

--- CompiletimeCrashTests/run-compiletime-crash-tests.sh
+++ CompiletimeCrashTests/run-compiletime-crash-tests.sh
@@ -7,8 +7,10 @@ if [[ $OSTYPE == 'darwin'* ]]; then
         export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
 fi
 
+# Use indexed arrays instead of associative arrays
+test_keys=()
+test_values=()
 
-declare -A test_results
 
 for folder in $(find . -type d -mindepth 1 -maxdepth 1 | sed 's|^\./||'); do
     let total_count+=1
@@ -18,8 +20,9 @@ for folder in $(find . -type d -mindepth 1 -maxdepth 1 | sed 's|^\./||'); do
     RETURN_CODE=$?
     echo "Finished building $folder"
     cd - > /dev/null # navigate back to previous folder
-    test_results[$folder]=$reproducer_type
     # script expected to succeed
+    test_keys+=("$folder")
+    test_values+=("$reproducer_type")
     if [ $RETURN_CODE -eq 0 ]; then
         # script succeeded
         echo -e "$folder: $reproducer_type\n"
@@ -35,14 +38,13 @@ done
 
 # format JSON
 json="{"
-first=1
-for test in "${!test_results[@]}"; do
-    value=${test_results[$test]}
-    if [[ $first -eq 0 ]]; then
+for i in "${!test_keys[@]}"; do
+    key=${test_keys[$i]}
+    value=${test_values[$i]}
+    if [ $i -ne 0 ]; then
         json+=", "
     fi
-    json+="\"$test\": \"$value\" "
-    first=0
+    json+="\"$key\": \"$value\""
 done
 json+="}"
 
