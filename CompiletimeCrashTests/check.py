f95cdeff135710deec7be78ee4fe4db30dca565b
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-05-28 15:20:20 +0200

    fix: print individual lines

--- CompiletimeCrashTests/check.py
+++ CompiletimeCrashTests/check.py
@@ -164,7 +164,7 @@ def main():
     ground_truth_filename = f"expected-{ground_truth_version}-{kernel_name}.txt"
     compiler_output = stdin.readlines()
     if 'nightly' in swift_version or 'snapshot' in swift_version:
-        errprint(compiler_output)
+        for i, line in enumerate(compiler_output): errprint("TRACE ", ':', line)
     expected_output = []
     with open(ground_truth_filename) as file:
         while line := file.readline():
