c496499f7ecd10c8fbecd8d3c34466814add9c92
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-06-02 17:10:52 +0530

    test: new variants of c003 and c016 with `@_alwaysEmitIntoClient`

---
+++ CompiletimeCrashTests/c031-nil-coalescing-silgen-crash-modified/main.swift
@@ -0,0 +1,18 @@
+import _Differentiation
+
+// https://github.com/swiftlang/swift/issues/55882
+// Compiler crashes for custom derivative of nil-coalescing operator.
+
+@derivative(of: ??)
+@usableFromInline
+@_alwaysEmitIntoClient
+func _vjpNilCoalescing<T: Differentiable>(optional: T?, defaultValue: @autoclosure () throws -> T)
+    rethrows -> (value: T, pullback: (T.TangentVector) -> Optional<T>.TangentVector)
+{
+    let hasValue = optional != nil
+    let value = try optional ?? defaultValue()
+    func pullback(_ v: T.TangentVector) -> Optional<T>.TangentVector {
+        return hasValue ? .init(v) : .zero
+    }
+    return (value, pullback)
+}
---
+++ CompiletimeCrashTests/c032-modified-custom-vjp-triggers-assertion-internal-unserializable/main.swift
@@ -0,0 +1,21 @@
+// https://github.com/swiftlang/swift/issues/58246
+// Regression: Custom VJP causes assertion failure: internal/private function cannot be serialized
+
+import _Differentiation
+
+extension SIMD
+where
+    Self: Differentiable,
+    Scalar: BinaryFloatingPoint & Differentiable,
+    Scalar.TangentVector: BinaryFloatingPoint,
+    TangentVector == Self
+{
+    @inlinable
+    @derivative(of: sum)
+    @_alwaysEmitIntoClient
+    public func _vjpSum() -> (
+        value: Scalar, pullback: (Scalar.TangentVector) -> TangentVector
+    ) {
+        return (sum(), { v in Self(repeating: Scalar(v)) })
+    }
+}
